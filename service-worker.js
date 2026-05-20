e8376272d1ed9ce08a6102ee9b5a9f8ca0d81fee
masuksen13-wq <masuk.sen13@gmail.com>
2026-05-20 22:33:15 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "dogam-randevu-v10";
-const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.json", "icon.svg", "logo.jpeg"];
+const CACHE_NAME = "dogam-randevu-v12";
+const ASSETS = ["./", "index.html", "styles.css", "app.js", "manifest.json", "icon.svg", "icon-192.png", "icon-512.png", "logo.jpeg"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
